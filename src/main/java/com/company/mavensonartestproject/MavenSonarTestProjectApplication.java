11b9e422ac4f8fcf8ee749b950a7a4fc3962b674
rajureddynimm123@gmail.com <Raju Reddy N>
2023-08-14 13:25:28 +0530

    some files created

---
+++ src/main/java/com/company/mavensonartestproject/MavenSonarTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.company.mavensonartestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSonarTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSonarTestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/mavensonartestproject/MavenSonarTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.mavensonartestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenSonarTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
